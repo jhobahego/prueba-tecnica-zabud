c3f1d5f6b3fb77dd8e8bccbf9296ae87415d49cf
Jhon Hernandez <jhobahego16@hotmail.com>
2023-12-24 18:08:06 -0500

    Remove Try catch innecesarios

--- backend/src/main/java/com/pruebatecnica/zabud/controllers/ProductoController.java
+++ backend/src/main/java/com/pruebatecnica/zabud/controllers/ProductoController.java
@@ -1,5 +1,6 @@
 package com.pruebatecnica.zabud.controllers;
 
+import com.pruebatecnica.zabud.models.entities.Producto;
 import com.pruebatecnica.zabud.models.entities.dto.ProductoRequest;
 import com.pruebatecnica.zabud.models.entities.dto.ProductoResponse;
 import com.pruebatecnica.zabud.services.impl.ProductoService;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @AllArgsConstructor
@@ -18,35 +20,23 @@ public class ProductoController {
     private final ProductoService productoService;
 
     @PostMapping
-    public ResponseEntity<?> crearProducto(@RequestBody ProductoRequest productoRequest) {
-        try {
-            ProductoResponse productoResponse = productoService.crearProducto(productoRequest);
+    public ResponseEntity<?> crearProducto(@RequestBody ProductoRequest productoRequest) throws URISyntaxException {
+        ProductoResponse productoResponse = productoService.crearProducto(productoRequest);
 
-            return ResponseEntity.created(new URI("/api/productos")).body(productoResponse);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.created(new URI("/api/productos")).body(productoResponse);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> actualizarProducto(@PathVariable Long id, @RequestBody ProductoRequest productoRequest) {
-        try {
-            ProductoResponse productoActualizado = productoService.actualizarProducto(id, productoRequest);
+        Producto productoActualizado = productoService.actualizarProducto(id, productoRequest);
 
-            return ResponseEntity.ok(productoActualizado);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok(productoActualizado);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarProducto(@PathVariable Long id) {
-        try {
-            productoService.eliminarProducto(id);
+        productoService.eliminarProducto(id);
 
-            return ResponseEntity.noContent().build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.noContent().build();
     }
 }
